fcd9e9f753fe4af04f979708eed641247fa74fde
chriskappa <chriskagkelis@yahoo.com>
2024-02-12 13:34:30 +0000

    [nativewind] Setup nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.jsx',
+    './**/*.tsx',
+  ],
+  theme: {
+    fontSize: {
+      eleven: '11px',
+      fifteen: '15px',
+      medium: '25px',
+      large: '35px',
+      xxl: '15px',
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
+    fontFamily: {
+      sans: ['"Open Sans"', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
