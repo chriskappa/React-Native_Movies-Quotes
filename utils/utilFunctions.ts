115f205498a2b89b4aabd8ac9ad8428adeeb362a
chriskappa <chriskagkelis@yahoo.com>
2024-02-12 14:44:16 +0000

    [UTILS] Added more functions to utils

--- utils/utilFunctions.ts
+++ utils/utilFunctions.ts
@@ -1,7 +1,12 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';;
+import { View, StyleSheet, ToastAndroid, Button, StatusBar } from 'react-native';
 
 type CustomDebounceProps = ((event: GestureResponderEvent | number) => void)
 
-
+export enum Storage {
+    FAVOURITE = 'favourite'
+}
 /*
 Function To create debounce functionality
 I will use it to optimize diffrent parts of the application and make the quotes more efficient
@@ -16,3 +21,17 @@ export const customDebounce = (fn: Function, timer: number): CustomDebounceProps
         }, timer);
     };
 };
+
+export const getRandomID = (): string | number[] => uuid.v4();
+
+
+export const SaveToLocalStorage = (key: string, value: string) => {
+    try {
+        AsyncStorage.setItem(key, value)
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+
+export const showToast = (message: string) => ToastAndroid.show(message, ToastAndroid.SHORT);
