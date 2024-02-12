445ce24a2ef74beebe3400f14669c27e865827ee
chriskappa <chriskagkelis@yahoo.com>
2024-02-12 16:45:27 +0000

    [UI] Added styled safeareaview

--- Design-System/Styled.tsx
+++ Design-System/Styled.tsx
@@ -1,14 +1,16 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, SafeAreaView } from 'react-native'
 import { styled } from 'nativewind';
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
 const StyledInput = styled(TextInput)
 const StyledTouchable = styled(TouchableOpacity)
+const StyledSafeAreaView = styled(SafeAreaView)
 const Styled = () => null
 
 Styled.View = StyledView;
 Styled.Text = StyledText;
 Styled.Input = StyledInput;
 Styled.Touchable = StyledTouchable
+Styled.SafeAreaView = StyledSafeAreaView;
 export default Styled
