81f7f3455da1fba0604740cb186bac80eccb68ad
chriskappa <chriskagkelis@yahoo.com>
2024-02-12 14:05:38 +0000

    [UTILS] Added default text for quotes

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,2 +1,5 @@
 export const API_URL = "https://655155de7d203ab6626ebfda.mockapi.io/api/quotes/quotes"
 export const MOCK_USER = { userName: "Admin", email: "admin@yahoo.com", userLoading: false }
+export const DEFAULT_INPUT_PLACEHOLDER = "Enter Your Quote";
+export const MQMAIN_DEFAULT_TEXTT = "Quotes List Is Empty";
+export const MQDEFAULT_SUB_TEXT = "Add new one by using the form";
